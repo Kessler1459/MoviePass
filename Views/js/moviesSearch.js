0dcc216b467b468a2c9cba2d959bba951dd29bc4
Kessler1459 <kessler1459@gmail.com>
2020-11-09 19:24:28 -0300

    correccion en parametros de datachange

--- Views/js/moviesSearch.js
+++ Views/js/moviesSearch.js
@@ -9,10 +9,11 @@ $(document).ready(function () {
                     "search": search
                 },
                 success: function (data) {
+                    console.log(data);
                     $("#moviesResponse").html("");
                     var parse = JSON.parse(data, null, 2);
                     $.each(parse, function (key, value) {
-                        var str = "<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('" + value["poster"] + "','" + value["title"] + "','" + value["synopsis"] + "')\" data-id=\"" + value["id"] + "\" data-toggle='modal' data-target='.movie' >";
+                        var str = "<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('" + value["poster"] + "','" + value["title"] + "','" + value["synopsis"] + "','"+value["id"]+"')\" data-id=\"" + value["id"] + "\" data-toggle='modal' data-target='.movie' >";
                         str += "<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w500" + value["poster"] + "\" width=60% ><figcaption class='figure-caption'>" + value["title"] + "</figcaption></figure>";
                         str += "</button></div>";
                         $("#moviesResponse").append(str);
--- Views/js/projFilter.js
+++ Views/js/projFilter.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
                 $("#moviesResponse").html("");
                 var parse=JSON.parse(data,null,2);
                 $.each(parse,function(key,value){
-                    var str="<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('"+value["movie"]["poster"]+"','"+value["movie"]["title"]+"','"+value["movie"]["synopsis"]+"')\" data-id=\""+value["movie"]["id"]+"\" data-toggle='modal' data-target='.movie' >";
+                    var str="<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('"+value["movie"]["poster"]+"','"+value["movie"]["title"]+"','"+value["movie"]["synopsis"]+"','"+value["movie"]["id"]+"')\" data-id=\""+value["movie"]["id"]+"\" data-toggle='modal' data-target='.movie' >";
                     str+="<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w500"+value["movie"]["poster"]+"\" width=60% ><figcaption class='figure-caption'>"+value["movie"]["title"]+"</figcaption></figure>";
                     str+="</button></div>";
                     $("#moviesResponse").append(str);         
