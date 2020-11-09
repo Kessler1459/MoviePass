d1301140e17a26d079433a5bc4dd702756310584
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-11-08 21:53:00 -0300

    Correccion de los try

--- DAO/UserDAO.php
+++ DAO/UserDAO.php
@@ -8,25 +8,26 @@
     class UserDAO
     {
         private $connection;
-        private $tableName = "users";
+        private $tableName;
 
         public function __construct() {
-            
+            $this->tableName = "users";
         }
 
         public function add($user){
+            
+            $query = "INSERT INTO $this->tableName (id_user,email,pass,first_name,last_name,dni,user_type,role_description) 
+                        VALUES (:id_user,:email,:pass,:first_name,:last_name,:dni,:user_type,:role_description)";
+            $parameters["id_user"] = $user->getId();
+            $parameters["email"] = $user->getEmail();
+            $parameters["pass"] = $user->getPassword();
+            $parameters["first_name"] = $user->getUserProfile()->getFirstName();
+            $parameters["last_name"] = $user->getUserProfile()->getLastName();
+            $parameters["dni"] = $user->getUserProfile()->getDni();
+            $parameters["user_type"] = $user->getUserRole()->getUserType();
+            $parameters["role_description"] = $user->getUserRole()->getDescription();
             try
             {
-                $query = "INSERT INTO $this->tableName (id_user,email,pass,first_name,last_name,dni,user_type,role_description) 
-                            VALUES (:id_user,:email,:pass,:first_name,:last_name,:dni,:user_type,:role_description)";
-                $parameters["id_user"] = $user->getId();
-                $parameters["email"] = $user->getEmail();
-                $parameters["pass"] = $user->getPassword();
-                $parameters["first_name"] = $user->userProfile->getFirstName();
-                $parameters["last_name"] = $user->userProfile->getLastName();
-                $parameters["dni"] = $user->userProfile->getDni();
-                $parameters["user_type"] = $user->userRole->getUserType();
-                $parameters["role_description"] = $user->userRole->getDescription();
                 $this->connection = Connection::getInstance();
                 $this->connection->executeNonQuery($query, $parameters);
             }
@@ -38,10 +39,18 @@
 
 
         public function getById($id_user){
-            try{
-                $query="SELECT * from $this->tableName where id_user=$id_user";
+            
+            $query="SELECT * from $this->tableName where id_user=$id_user";
+            
+            try
+            {    
                 $this->connection=Connection::getInstance();
                 $results=$this->connection->execute($query);
+
+            }catch(Exception $ex){
+                throw $ex;
+            }
+            
                 if(!empty($results)){
                     $row=$results[0];
 
@@ -55,17 +64,22 @@
                     return false;
                 }
                 
-            }catch(Exception $ex){
-                throw $ex;
-            }
         }
 
 
         public function findUser($email,$password){
-            try{
-                $query="SELECT * from $this->tableName where email=$email AND pass=$password";
+            
+            $query="SELECT * from $this->tableName where email=$email AND pass=$password";
+            
+            try
+            {
                 $this->connection=Connection::getInstance();
                 $results=$this->connection->execute($query);
+
+            }catch(Exception $ex){
+                throw $ex;
+            }
+
                 if(!empty($results)){
                     $row=$results[0];
 
@@ -78,18 +92,21 @@
                 else{
                     return false;
                 }
-                
-            }catch(Exception $ex){
-                throw $ex;
-            }
         }
 
 
         public function findEmail($email){
-            try{
-                $query="SELECT * from $this->tableName where email=$email";
+            
+            $query="SELECT * from $this->tableName where email=$email";
+            
+            try
+            {
                 $this->connection=Connection::getInstance();
                 $results=$this->connection->execute($query);
+            
+            }catch(Exception $ex){
+                throw $ex;
+            }   
                 if(!empty($results)){
                     
                     return true;
@@ -97,10 +114,6 @@
                 else{
                     return false;
                 }
-                
-            }catch(Exception $ex){
-                throw $ex;
-            }
         }
     
     }
