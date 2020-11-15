d7612b058782301fd5417c858549270d04b5931c
Kessler1459 <kessler1459@gmail.com>
2020-11-15 14:51:06 -0300

    cambio de tamaño portadas

--- Views/js/moviesSearch.js
+++ Views/js/moviesSearch.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
                     var parse = JSON.parse(data, null, 2);
                     $.each(parse, function (key, value) {
                         var str = "<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('" + value["poster"] + "','" + value["title"] + "','" + value["synopsis"] + "','"+value["id"]+"')\" data-id=\"" + value["id"] + "\" data-toggle='modal' data-target='.movie' >";
-                        str += "<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w500" + value["poster"] + "\" width=60% ><figcaption class='figure-caption'>" + value["title"] + "</figcaption></figure>";
+                        str += "<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w154" + value["poster"] + "\" width=60% ><figcaption class='figure-caption'>" + value["title"] + "</figcaption></figure>";
                         str += "</button></div>";
                         $("#moviesResponse").append(str);
                     })
--- Views/js/projFilter.js
+++ Views/js/projFilter.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
                 var parse=JSON.parse(data,null,2);
                 $.each(parse,function(key,value){
                     var str="<div class='col-md-3'><button type='button' class='btn' onClick=\"dataChange('"+value["movie"]["poster"]+"','"+value["movie"]["title"]+"','"+value["movie"]["synopsis"]+"','"+value["movie"]["id"]+"')\" data-id=\""+value["movie"]["id"]+"\" data-toggle='modal' data-target='.movie' >";
-                    str+="<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w500"+value["movie"]["poster"]+"\" width=60% ><figcaption class='figure-caption'>"+value["movie"]["title"]+"</figcaption></figure>";
+                    str+="<figure class='figure'><img class='figure-img img-fluid rounded' src=\"https://image.tmdb.org/t/p/w154"+value["movie"]["poster"]+"\" width=60% ><figcaption class='figure-caption'>"+value["movie"]["title"]+"</figcaption></figure>";
                     str+="</button></div>";
                     $("#moviesResponse").append(str);         
                 })
