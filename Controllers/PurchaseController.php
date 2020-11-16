82f4b8238e9cb7278eb778ee044627f35e80070a
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-11-16 13:05:52 -0300

    PurchaseController con su metodo  processPurchse

--- Controllers/PurchaseController.php
+++ Controllers/PurchaseController.php
@@ -5,27 +5,65 @@
     use DAO\PurchaseDAO as PurchaseDAO;
     use Controllers\TicketController as TicketController;
     use Controllers\ProjectionController as ProjectionController;
+    use Controllers\DiscountController as DiscountController;
+    use Controllers\CreditAccountController as CreditAccountController;
     use Models\Projection as Projection;
     use Models\Movie as Movie;
     use Models\Room as Room;
     use Models\Cinema as Cinema;
     use \Exception as Exception;
 
+
     class PurchaseController{
         private $purchaseDAO;
+        private $ticketController;
+        private $projectionController;
+        private $discountController;
+        private $creditAccountController;
 
         public function __construct() {
             $this->purchaseDao = new PurchaseDao();
+            $this->ticketController = new TicketController();
+            $this->projectionController = new ProjectionController();
+            $this->discountController = new DiscountController();
+            $this->creditAccountController = new CreditAccountController();
         }
         
+        public function processPurchse($quantity_tickets,$id_proj){
+            $soldTickets = $this->ticketController->getByProjId($idProj);
+            $proj = $this->$projectionController->getById($id_proj);
+            $capacity = $proj->getRoom()->getCapacity();
 
-        public function selectProj($id_proj){
-            $projectionController = new ProjectionController();
-            $result = $projectionController->getById($id_proj);
-            
-            $proj = $result;
-            $room = $result->getRoom();
-            $movie = $result->getMovie();
+            $availableTickets = $capacity - $soldTickets;
+
+            if ($quantity_tickets > 0 && $availableTickets >= $quantity_tickets) {
+                $date = date("yy-m-d");
+
+                $discountsArray = $this->discountController->getByDate($date);
+                $creditAccounts = $this->creditAccountController->getAll();
+                
+                if ($discountsArray == null) {
+                    $discountsArray = array();
+                }
+            }
+            else {
+                if($quantity_tickets = 0){
+                    $message = "Error the number of tickets to buy is 0";
+                }
+                else{
+                    $title = $proj->getMovie()->getTitle();
+                    $message = "There are only $availableTickets tickets available for $title";
+                }
+                    
+                $this->selectProj($id_proj,$message);
+            }
+        }
+
+        public function selectProj($id_proj,$message=""){
+            $proj = $this->$projectionController->getById($id_proj);
+        
+            $room = $proj->getRoom();
+            $movie = $proj->getMovie();
             $cinema = $room->getCinema();
             
             include VIEWS_PATH."purchase_form.php";
@@ -45,7 +83,7 @@
                 $message="Error adding the purchase.";
                 include(VIEWS_PATH."message_view.php");
             }
-            $ticketController = new TicketController();
+            
             for ($i=0; $i <  $quantity_tickets; $i++) { 
                 $ticketController->add($num,$idProj,$id_purchase);
             }    
