ae1fbaff122cd17ab37bb806ee4d3172ea44ed15
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-11-16 17:01:14 -0300

    Se agregan metodos de finalizar compra

--- Controllers/PurchaseController.php
+++ Controllers/PurchaseController.php
@@ -7,10 +7,14 @@
     use Controllers\ProjectionController as ProjectionController;
     use Controllers\DiscountController as DiscountController;
     use Controllers\CreditAccountController as CreditAccountController;
+    use Controllers\PaymentCCController as PaymentCCController;
     use Models\Projection as Projection;
     use Models\Movie as Movie;
     use Models\Room as Room;
     use Models\Cinema as Cinema;
+    use Models\Purchase as Purchase;
+    use Models\Ticket as Ticket;
+    use Models\PaymentCC as PaymentCC;
     use \Exception as Exception;
 
 
@@ -20,6 +24,7 @@
         private $projectionController;
         private $discountController;
         private $creditAccountController;
+        private $paymentCCController;
 
         public function __construct() {
             $this->purchaseDao = new PurchaseDao();
@@ -27,6 +32,7 @@
             $this->projectionController = new ProjectionController();
             $this->discountController = new DiscountController();
             $this->creditAccountController = new CreditAccountController();
+            $this->paymentCCController = new PaymentCCController();
         }
         
         public function processPurchase($quantity_tickets,$id_proj){
@@ -60,6 +66,8 @@
             }
         }
 
+
+
         public function selectProj($id_proj,$message=""){
             $proj = $this->projectionController->getById($id_proj);
         
@@ -70,6 +78,44 @@
             include VIEWS_PATH."purchase_form.php";
         }
 
+        public function completPurchase($id_creditAccount,$aut_cod,$cardNumber,$quantity_tickets,$id_proj){
+            $proj = $this->projectionController->getById($id_proj);
+            $ticketPrice = $proj->getRoom()->getTicketPrice();
+            $total = $quantity_tickets * $ticketPrice;
+            $date = date("yy-m-d");
+            $discountOBJ=$this->discountController->getByDateAccount($date,$accountId);
+            $porc_discount = $discountOBJ->getPercent();
+            $final = ($total / 100 ) * (100 - $porc_discount);
+
+            $ticketsArray = $this->sendPurchase($id_creditAccount,$quantity_tickets,$porc_discount,$total,$aut_cod);
+
+            include VIEWS_PATH."sold_tickets.php";           
+        }
+
+
+        public function sendPurchase($id_creditAccount,$quantity_tickets,$discount,$total,$aut_code) {
+            $soldTickets = $this->ticketController->getByProjId($id_proj);
+            $proj = $this->projectionController->getById($id_proj);
+            $capacity = $proj->getRoom()->getCapacity();
+
+            $availableTickets = $capacity - $soldTickets ;//Usar soldTickets ++ para num asiento
+ 
+            $id_purchase = $this->add($quantity_tickets,$discount,$date,$total);
+            
+            $this->paymentCCController->add($id_creditAccount,$aut_code,$date,$total)
+
+            $ticketsArray = array();
+
+            for ($i=0; $i <  $quantity_tickets; $i++) { 
+                $soldTickets++;
+                $idTicket = $ticketController->add($soldTickets,$idProj,$id_purchase);
+                $ticket = new Ticket($idTicket,$soldTickets);
+                array_push($ticketsArray, $value);
+            }
+            
+            return $ticketsArray;
+        }
+
         public function add($quantity_tickets,$discount,$date,$total) {
             if(session_status () != 2){
                 session_start();  
@@ -77,20 +123,18 @@
             $id_user = $_SESSION['Id'];
             try
             {   
-                $id_purchase = $this->add($id_user,$quantity_tickets,$discount,$date,$total);
+                $id_purchase = $this->purchaseDAO->add($id_user,$quantity_tickets,$discount,$date,$total);
             }
             catch(Exception $ex)
             {
                 $message="Error adding the purchase.";
                 include(VIEWS_PATH."message_view.php");
             }
-            
-            for ($i=0; $i <  $quantity_tickets; $i++) { 
-                $ticketController->add($num,$idProj,$id_purchase);
-            }    
-                
+            return $id_purchase;
             
         }
+
+        
 
         public function getById($id_ticket){
             $id_purchase = 0;// con el ticket se busca id compra y se pasa por aca
