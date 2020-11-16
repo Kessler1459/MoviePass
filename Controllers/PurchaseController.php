501b73c57e504caf79546b8da82dced027e3aa9b
Nahuel Flores <66324788+Nahuel-Flores@users.noreply.github.com>
2020-11-16 01:30:30 -0300

    Se agregan metodos

--- Controllers/PurchaseController.php
+++ Controllers/PurchaseController.php
@@ -3,23 +3,52 @@
     namespace Controllers;
 
     use DAO\PurchaseDAO as UserDAO;
-
+    use Controllers\TicketController as TicketController;
+    use Controllers\ProjectionController as ProjectionController;
+    use Models\Projection as Projection;
+    use Models\Movie as Movie;
+    use Models\Room as Room;
+    use Models\Cinema as Cinema;
     class PurchaseController{
         private $purchaseDAO;
 
         public function __construct() {
             $this->purchaseDao = new PurchaseDao();
         }
+        
 
-        public function add($quantity_tikets,$discount,$date,$total) {
+
+        public function selectProj($id_proj){
+            $projectionController = new ProjectionController();
+            $result = $projectionController->getById($id_proj);
+            
+            $proj = $result;
+            $room = $result->getRoom();
+            $movie = $result->getMovie();
+            $cinema = $room->getCinema();
+            
+            include VIEWS_PATH."purchase_form.php";
+        }
+
+        public function add($quantity_tickets,$discount,$date,$total) {
+            if(session_status () != 2){
+                session_start();  
+            }
+            $id_user = $_SESSION['Id'];
             try
-            {
-                $this->add($quantity_tikets,$discount,$date,$total);
+            {   
+                $id_purchase = $this->add($id_user,$quantity_tickets,$discount,$date,$total);
             }
             catch(Exception $ex)
             {
                 throw $ex;
             }
+            $ticketController = new TicketController();
+            for ($i=0; $i <  $quantity_tickets; $i++) { 
+                $ticketController->add($num,$idProj,$id_purchase);
+            }    
+                
+            
         }
 
         public function getById($id_ticket){
